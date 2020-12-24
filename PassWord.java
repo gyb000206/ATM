7b775116554537f7a4321c62dfd720c824d8f4cd
gyb000206 <60536348+gyb000206@users.noreply.github.com>
2020-12-24 20:38:53 +0800

    Update PassWord.java

--- PassWord.java
+++ PassWord.java
@@ -1,27 +1,26 @@
 package date;
-
+/**
+ * @author 高宇博
+ * @ID 2503190213
+ */
 public class PassWord {
-    private String PassWord = "123456";
-    private String User = "123456";
-    final private String administrator = "admin";
-
-    public void setUser(String user) {
-        User = user;
-    }
+    private static String PassWord = "123456";
+    private static final String User = "123456";
+    private final String administrator = "admin";
 
     public void setPassWord(String passWord) {
         PassWord = passWord;
     }
 
-    public String getUser() {
-        return User;
-    }
-
-    public String getPassWord() {
+    public static String getPassWord() {
         return PassWord;
     }
 
     public String getAdministrator() {
         return administrator;
+    }
+
+    public static String getUser() {
+        return User;
     }
 }
