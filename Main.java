e86aa3a4efc963ab120cc0421fcf107987249e05
gyb000206 <60536348+gyb000206@users.noreply.github.com>
2020-12-24 20:35:09 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 package view;
-
 /**
  * @author 高宇博
  * @ID 2503190213
- * 主函数
- * 程序入口
  */
-public class Main {
+
+public class Main{
     public static void main(String[] args) {
-        new Login();
+        new Login().Login();
     }
 }
